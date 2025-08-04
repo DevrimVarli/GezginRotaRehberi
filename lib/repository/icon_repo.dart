55a3f3836110349a633dca4ef9806cf61982704b
DevrimVarli <Devrimvarli571@gmail.com>
2025-08-04 09:36:50 +0300

    feat:gist added

--- lib/repository/icon_repo.dart
+++ lib/repository/icon_repo.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:yeni_tasarim/model/Imkanlar.dart';
 
 /// İmkan verilerini assets/api.json dosyasından okuyan repository sınıfı
 class ImkanRepo {
-  /// assets/api.json içindeki 'imkanlar' listesini Imkanlar model listesine dönüştürür
+ /* /// assets/api.json içindeki 'imkanlar' listesini Imkanlar model listesine dönüştürür
   Future<List<Imkanlar>> imkanlar() async {
     // JSON dosyasını assets klasöründen yükle
     String jsonString = await rootBundle.loadString('assets/api.json');
@@ -20,5 +21,34 @@ class ImkanRepo {
         .toList();
 
     return imkanlarList;
+  }*/
+  final String _url =
+      'https://gist.githubusercontent.com/DevrimVarli/d98d5353665d50d947ea321840b442d3/raw/c496abc69f9f3eb0bc006e8617d7ef2892971d4e/data.json';
+
+  Future<List<Imkanlar>> imkanlar() async {
+    try {
+      Response<dynamic> response = await Dio().get(_url);
+
+      if (response.statusCode == 200) {
+        // Gelen veri String olabilir, decode et
+        var rawData = response.data;
+        Map<String, dynamic> data = rawData is String
+            ? jsonDecode(rawData) as Map<String, dynamic>
+            : rawData as Map<String, dynamic>;
+
+        // "imkanlar" listesi var mı kontrol et
+        if (data['imkanlar'] is List) {
+          return (data['imkanlar'] as List<dynamic>)
+              .map((e) => Imkanlar.fromJson(e as Map<String, dynamic>))
+              .toList();
+        } else {
+          throw Exception('İmkan listesi bulunamadı.');
+        }
+      } else {
+        throw Exception('Sunucu hatası: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('İmkan verileri alınamadı: $e');
+    }
   }
 }
--- lib/repository/otel_repo.dart
+++ lib/repository/otel_repo.dart
@@ -1,9 +1,39 @@
 import 'dart:convert';
-import 'package:flutter/services.dart';
+import 'package:dio/dio.dart';
 import 'package:yeni_tasarim/model/oteller.dart';
 
 /// Otel verilerini assets/api.json dosyasından okuyan repository sınıfı
 class OtelRepo {
+  final String _url =
+      'https://gist.githubusercontent.com/DevrimVarli/d98d5353665d50d947ea321840b442d3/raw/c496abc69f9f3eb0bc006e8617d7ef2892971d4e/data.json';
+
+  Future<List<Oteller>> oteller() async {
+    try {
+      Response<dynamic> response = await Dio().get(_url);
+
+      if (response.statusCode == 200) {
+        // Gelen veri String olabilir, decode et
+        var rawData = response.data;
+        Map<String, dynamic> data = rawData is String
+            ? jsonDecode(rawData) as Map<String, dynamic>
+            : rawData as Map<String, dynamic>;
+
+        // "oteller" listesi var mı kontrol et
+        if (data['oteller'] is List) {
+          return (data['oteller'] as List<dynamic>)
+              .map((e) => Oteller.fromJson(e as Map<String, dynamic>))
+              .toList();
+        } else {
+          throw Exception('Otel listesi bulunamadı.');
+        }
+      } else {
+        throw Exception('Sunucu hatası: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Otel verileri alınamadı: $e');
+    }
+  }
+  /*
   /// assets/api.json içindeki 'oteller' listesini Oteller model listesine dönüştürür
   Future<List<Oteller>> oteller() async {
     // JSON dosyasını assets klasöründen yükle
@@ -19,5 +49,5 @@ class OtelRepo {
         .toList();
 
     return otellerList;
-  }
+  }*/
 }
