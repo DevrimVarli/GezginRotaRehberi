bd7f93413ecfff17bc0aa2508fa63be11211688f
DevrimVarli <Devrimvarli571@gmail.com>
2025-09-10 14:14:20 +0300

    feat:package install

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: 'https://DevrimVarli.github.io', // kendi kullanıcı adın
+  base: '/GezginRotaRehberiAstro/',           // repo adınla birebir aynı
   integrations: [tailwind(), react()],
 });
